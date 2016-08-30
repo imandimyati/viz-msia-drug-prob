e433e746b1b59649b0b862bc251bf0cdbeffa3f7
imandimyati <ImanDimyati@Nuruls-MacBook-Pro.local>
2016-08-30 16:19:45 +0100

    update data analysis for main article

--- datasets_csv/aadk_cost_analysis.py
+++ datasets_csv/aadk_cost_analysis.py
@@ -8,12 +8,21 @@ import matplotlib.pyplot as	plt
 
 # import the csv file
 all_data = pd.read_csv('aadk_cost.csv')
+# all_data = pd.read_csv('seizure_value.csv')
+
 all_data = all_data.dropna()
 #print(all_data)
-
 print(all_data.columns)
-print(all_data.describe())
+# print(all_data.describe())
+
+# aadk spending
 print(all_data['Cost (RM)'])
 print(all_data['Total Cost (RM)'])
 print(all_data[all_data['Purpose'] == 'Development'].describe())
 print(all_data[all_data['Purpose'] == 'Operation'].describe())
+
+# seizure value
+# print(all_data[all_data['Year'] == 2013].describe())
+# print(all_data[all_data['Year'] == 2014].describe())
+# print(all_data[all_data['Type of Drug'] == 'Heroin'].describe())
+# print(all_data[all_data['Type of Drug'] == 'Marijuana'].describe())
--- datasets_csv/puspen_cost_analysis.py
+++ datasets_csv/puspen_cost_analysis.py
@@ -7,14 +7,18 @@ import scipy.stats as stats
 import matplotlib.pyplot as	plt
 
 # import the csv file
-all_data = pd.read_csv('puspen_cost.csv')
+# all_data = pd.read_csv('puspen_cost.csv')
+# all_data = pd.read_csv('mmt_cost.csv')
+all_data = pd.read_csv('nsp_cost.csv')
+
 all_data = all_data.dropna()
 #print(all_data)
-
 print(all_data.columns)
 print(all_data.describe())
-print(all_data['Number of Residents'])
-print(all_data['Cost Estimate (RM)'])
+
+# puspen cost
+# print(all_data['Number of Residents'])
+# print(all_data['Cost Estimate (RM)'])
 
 # Difference between the years for Cost Estimate and Number of Residents
 print(all_data.set_index('Year').diff())
